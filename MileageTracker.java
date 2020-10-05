491e35da19100ecca114f9ab4bf372bf37ce20ed
Matt Callaghan <mcallaghan@naperville203.org>
2020-10-05 00:29:58 -0500

    Finished Day 1 Notes

--- MileageTracker.java
+++ MileageTracker.java
@@ -2,17 +2,97 @@
 /**
  * This class models a mileage tracker for a car.
  *
- * @author gcschmit
- * @version 27 September 2020
+ * @author mrcallaghan
+ * @version 05 October 2020
  */
 public class MileageTracker
 {
+    /*
+     * 2. Define the instance variables (store the object's attributes):
+     *      specify the visibility (e.g., private)
+     *          public: accessible by any code in any class
+     *          private: only accessible by methods in this class
+     *      specify the type (e.g., double)
+     *      specify the name (e.g., milesDriven)
+     *      
+     *      Instance variables differ from local variables in the following ways:
+     *      scoped to the class (accessible in all methods of the class;
+     *          lifetime is the same as the object)
+     *      automatically initialized to a default value (0, false, null)
+     *      best practice is not to immediately initialize instance variables
+     */
+    private double milesDriven;  // in units of miles
+    private double fuelConsumed; // in units of gallons
+    private String vin; // vehicle identification number
     
+    /*
+     * 3. Define the constructor(s):
+     *      responsible for initializing newly created objects
+     *      invoked automatically via the new operator
+     *      name of the constructor must match the name of the class
+     *      has no return type (not even void)
+     *      multiple constructors may be defined for a class
+     *      one constructor may call another constructor (with restrictions)
+     */
     
+    /**
+     * Default constructor for the MileageTracker class.
+     *      Initializes this the miles driven and fuel consumed to 0
+     *          and the VIN to null.
+     */
+    public MileageTracker()
+    {
+        /*
+         * The "this" reserved word references the current object (like "self" in Python).
+         * 
+         *  Its usage is encouraged but not always required.
+         */
+        this.milesDriven = 0;
+        this.fuelConsumed = 0;
+        this.vin = null;
+    }
     
+    /**
+     * Constructs a new MileageTracker object with the specified miles driven and fuel consumed.
+     * 
+     * @param initialMilesDriven the number of miles already driven
+     * @param initialFuelConsumed the number of gallons of fuel already consumed
+     */
+    public MileageTracker(double initialMilesDriven, double initialFuelConsumed)
+    {
+        this.milesDriven = initialMilesDriven;
+        this.fuelConsumed = initialFuelConsumed;
+        this.vin = null;
+    }
     
+    /*
+     * 1. Define methods by specifying:
+     *      the visibility (e.g., public)
+     *      the return type (e.g., void)
+     *      the method name (e.g., incrementMilesDriven)
+     *      the parameters and their types (e.g., miles of type double)
+     */
     
+    /**
+     * Increment the number of miles that the car has driven.
+     * 
+     * @param miles  the additional distance, in miles, this car has driven
+     */
+    public void incrementMilesDriven(double miles)
+    {
+        //!!! TO DO: implement later
+    }
     
+    /**
+     * Returns the total number of miles driven
+     * 
+     * @return the total number of miles driven
+     */
+    public double getMilesDriven()
+    {
+        //!!! TO DO: implement later
+        return 0.0;
+    }
     
     /**
      * Increment the number of gallons of fuel that this car has consumed
@@ -21,9 +101,9 @@ public class MileageTracker
      */
     public void incrementFuelConsumed(double gallons)
     {
-        
+
     }
-    
+
     /**
      * Returns the total number of gallons of fuel consumed
      * 
@@ -33,8 +113,7 @@ public class MileageTracker
     {
         return 0.0;
     }
-    
-    
+
     /**
      * Returns the current mileage, in miles per gallon
      * 
@@ -44,7 +123,7 @@ public class MileageTracker
     {
         return 0.0;
     }
-    
+
     /**
      * Returns the vehicle identification (VIN) of this car.
      * 
@@ -54,7 +133,7 @@ public class MileageTracker
     {
         return null;
     }
-    
+
     /**
      * Sets the vehicle identification (VIN) of this car.
      * 
@@ -65,9 +144,6 @@ public class MileageTracker
         vin = vin;
     }
 }
-
-
-
 
 
 
